ecfc9b69dc8b5dbe8729338f923f9820ee2f934e
Alan-Webb <webbalan91939@gmail.com>
2024-06-23 04:17:50 +0100

    Complete HTML and CSS, work on JS

---
+++ JSgames/candy-crush-clone/index.js
@@ -0,0 +1,18 @@
+const candies = ["blue", "Orange", "Green", "Yellow", "Red", "Purple"];
+const board = [];
+const rows = 9;
+const columns = 9;
+let score = 0;
+
+window.onload = function () {
+  startGame();
+}
+
+function startGame() {
+  for (let r = 0; r < rows; r++) {
+    let row = [];
+    for (let c = 0; c < columns; c++) {
+        let tile = document.createElement("img");
+    }
+  }
+}
