6b2b8b685c2c036803e8cb996efcc15473a96f7b
Alan-Webb <webbalan91939@gmail.com>
2024-06-28 21:23:04 +0100

    Complete space invaders clone

--- JSgames/space-invaders/index.js
+++ JSgames/space-invaders/index.js
@@ -4,8 +4,8 @@ let rows = 16;
 let columns = 16;
 
 let board;
-let boardWidth = tileSize * columns;
-let boardHeight = tileSize * rows;
+let boardWidth = tileSize * columns; 
+let boardHeight = tileSize * rows; 
 let context;
 
 /* PLAYER SHIP */
@@ -15,14 +15,14 @@ let shipX = tileSize * columns / 2 - tileSize;
 let shipY = tileSize * rows - tileSize * 2;
 
 let ship = {
-  x : shipX,
-  y : shipY,
-  width : shipWidth,
-  height : shipHeight
+    x : shipX,
+    y : shipY,
+    width : shipWidth,
+    height : shipHeight
 }
 
 let shipImg;
-shipMovementX = tileSize;
+let shipVelocityX = tileSize;
 
 /* INVADERS */
 let invaderArray = [];
@@ -34,107 +34,168 @@ let invaderImg;
 
 let invaderRows = 2;
 let invaderColumns = 3;
-let invaderCount = 0;
-let invaderMovementX = 1;
+let invaderCount = 0; 
+let invaderVelocityX = 1; 
 
 /* LASERS */
 let laserArray = [];
-let laserMovementY = -10;
+let laserVelocityY = -10;
 
 let score = 0;
 let gameOver = false;
 
 window.onload = function() {
-  board = document.getElementById("board");
-  board.width = boardWidth;
-  board.height = boardHeight;
-  context = board.getContext("2d");
+    board = document.getElementById("board");
+    board.width = boardWidth;
+    board.height = boardHeight;
+    context = board.getContext("2d");
 
+    //image loading
+    shipImg = new Image();
+    shipImg.src = "./images/ship.png";
+    shipImg.onload = function() {
+        context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
+    }
 
-  /* IMAGE LOADING */
-  shipImg = new Image();
-  shipImg.src = "./images/ship.png";
-  shipImg.onload = function() {
-    context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
-  }
+    invaderImg = new Image();
+    invaderImg.src = "./images/invader.png";
+    createInvaders();
 
-  invaderImg = new Image();
-  invaderImg.src = "./images/invader.png";
-  createInvaders();
-
-  requestAnimationFrame(update);
-  document.addEventListener("keydown", moveShip);
-  document.addEventListener("keyup", shoot);
+    requestAnimationFrame(update);
+    document.addEventListener("keydown", moveShip);
+    document.addEventListener("keyup", shoot);
 }
 
 function update() {
-  requestAnimationFrame(update);
+    requestAnimationFrame(update);
 
-  if (gameOver) {
-    return;
-  }
+    if (gameOver) {
+        return;
+    }
 
-  context.clearRect(0, 0, board.width, board.height);
+    context.clearRect(0, 0, board.width, board.height);
 
-  context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
+    //ship
+    context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
 
-  for (let i = 0; i < invaderArray.length; i++) {
-    let invader = invaderArray[i];
-    if (invader.alive) {
-      invader.x += invaderMovementX;
+    //invader
+    for (let i = 0; i < invaderArray.length; i++) {
+        let invader = invaderArray[i];
+        if (invader.alive) {
+            invader.x += invaderVelocityX;
 
-      if (invader.x + invader.width >= board.width || invader.x <= 0) {
-        invaderMovementX *= -1;
-        invader.x += invaderMovementX * 2;
+            if (invader.x + invader.width >= board.width || invader.x <= 0) {
+                invaderVelocityX *= -1;
+                invader.x += invaderVelocityX*2;
 
-        for (let j = 0; j < invaderArray.length; j ++) {
-          invaderArray[j].y += invaderHeight;
+                for (let j = 0; j < invaderArray.length; j++) {
+                    invaderArray[j].y += invaderHeight;
+                }
+            }
+            context.drawImage(invaderImg, invader.x, invader.y, invader.width, invader.height);
+
+            if (invader.y >= ship.y) {
+                gameOver = true;
+            }
         }
-      }
-      context.drawImage(invaderImg, invader.x, invader.y, invader.width, invader.height);
     }
-  }
+
+    //lasers
+    for (let i = 0; i < laserArray.length; i++) {
+        let laser = laserArray[i];
+        laser.y += laserVelocityY;
+        context.fillStyle="white";
+        context.fillRect(laser.x, laser.y, laser.width, laser.height);
+
+        for (let j = 0; j < invaderArray.length; j++) {
+            let invader = invaderArray[j];
+            if (!laser.used && invader.alive && detectCollision(laser, invader)) {
+                laser.used = true;
+                invader.alive = false;
+                invaderCount--;
+                score += 100;
+            }
+        }
+    }
+
+    while (laserArray.length > 0 && (laserArray[0].used || laserArray[0].y < 0)) {
+        laserArray.shift();
+    }
+
+    //refresh invaders
+    if (invaderCount == 0) {
+        score += invaderColumns * invaderRows * 100;
+        invaderColumns = Math.min(invaderColumns + 1, columns/2 -2);
+        invaderRows = Math.min(invaderRows + 1, rows-4);
+        if (invaderVelocityX > 0) {
+            invaderVelocityX += 0.2; 
+        }
+        else {
+            invaderVelocityX -= 0.2;
+        }
+        invaderArray = [];
+        laserArray = [];
+        createInvaders();
+    }
+
+    //score
+    context.fillStyle="white";
+    context.font="16px courier";
+    context.fillText(score, 5, 20);
 }
 
-
-
-/* PLAYER SHIP MOVEMENT */
 function moveShip(e) {
-  if (e.code == "ArrowLeft" && ship.x - shipMovementX >= 0) {
-    ship.x -= shipMovementX;
-  } else if (e.code == "ArrowRight" && ship.x + shipMovementX + ship.width <= board.width) {
-    ship.x += shipMovementX;
-  }
+    if (gameOver) {
+        return;
+    }
+
+    if (e.code == "ArrowLeft" && ship.x - shipVelocityX >= 0) {
+        ship.x -= shipVelocityX; 
+    }
+    else if (e.code == "ArrowRight" && ship.x + shipVelocityX + ship.width <= board.width) {
+        ship.x += shipVelocityX; 
+    }
 }
 
-/* INVADER SPAWNING */
 function createInvaders() {
-  for (let c = 0; c < invaderColumns; c++) {
-    for (let r = 0; r < invaderRows; r++) {
-      let invader = {
-        img : invaderImg,
-        x : invaderX + c * invaderWidth,
-        y : invaderY + r * invaderHeight,
-        width : invaderWidth,
-        height : invaderHeight,
-        alive : true
-      }
-      invaderArray.push(invader);
+    for (let c = 0; c < invaderColumns; c++) {
+        for (let r = 0; r < invaderRows; r++) {
+            let invader = {
+                img : invaderImg,
+                x : invaderX + c * invaderWidth,
+                y : invaderY + r * invaderHeight,
+                width : invaderWidth,
+                height : invaderHeight,
+                alive : true
+            }
+            invaderArray.push(invader);
+        }
     }
-  }
-  invaderCount = invaderArray.length;
+    invaderCount = invaderArray.length;
 }
 
-/* SHOOT LASERS */
+/* SHOOTING AND COLLISION */
 function shoot(e) {
-  if (e.code == "space") {
-    let laser = {
-      x : ship.x + shipWidth * 15 / 32;
-      y : ship.y,
-      width : tileSize / 8,
-      height : tileSize / 2,
-      used : false
+    if (gameOver) {
+        return;
     }
-    laserArray.push(laser);
-  }
+
+    if (e.code == "Space") {
+        
+        let laser = {
+            x : ship.x + shipWidth * 15/32,
+            y : ship.y,
+            width : tileSize / 8,
+            height : tileSize / 2,
+            used : false
+        }
+        laserArray.push(laser);
+    }
+}
+
+function detectCollision(a, b) {
+    return a.x < b.x + b.width &&   
+           a.x + a.width > b.x &&   
+           a.y < b.y + b.height &&  
+           a.y + a.height > b.y;    
 }
