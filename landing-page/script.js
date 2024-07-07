7f2656d6054ea157a4a7da26ec00090c537d8932
Alan-Webb <webbalan91939@gmail.com>
2024-07-07 19:40:30 +0100

    Add flex properties to layout

--- landing-page/script.js
+++ landing-page/script.js
@@ -1,7 +1,10 @@
+/* NAVBAR */
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-
+//hamburger animation
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+
