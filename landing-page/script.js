34bb7692916e1188fe0c43e321302da2744d1d4a
Alan Webb <webbalan91939@gmail.com>
2024-07-15 00:03:50 +0100

    Reorganise files

--- landing-page/script.js
+++ landing-page/script.js
@@ -6,3 +6,44 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+/* IMAGE CAROUSEL */
+const imgs = document.getElementById("imgs");
+const leftBtn = document.getElementById("prevBtn");
+const rightBtn = document.getElementById("nextBtn");
+const img = document.querySelectorAll("#imgs img");
+
+let idx = 0;
+
+let interval = setInterval(run, 3000);
+
+function run() {
+  idx++;
+  changeImage();
+}
+
+function changeImage() {
+  if(idx > img.length - 1) {
+    idx = 0;
+  } else if(idx < 0) {
+    idx = img.length - 1;
+  }
+  imgs.style.transform = `translateX(${-idx * 500}px)`;
+}
+
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(run, 3000);
+}
+
+nextBtn.addEventListener("click", () => {
+  idx++;
+  changeImage();
+  resetInterval();
+});
+
+prevBtn.addEventListener("click", () => {
+  idx--;
+  changeImage();
+  resetInterval();
+});
